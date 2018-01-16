0b7877ddc47b132c481005028ab098c7d3a3e9bc
Pendota <703402887@sanfordhealth.org>
2018-01-16 15:24:34 -0600

    initial commit

---
+++ com.skumar/src/main/java/com/skumar/jenkins/JenkinsDemo.java
@@ -0,0 +1,12 @@
+package com.skumar.jenkins;
+
+public class JenkinsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Kumar! Welcome to Jenkins!!");
+
+	}
+
+}
